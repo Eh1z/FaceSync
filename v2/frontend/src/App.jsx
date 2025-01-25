d25091a04589fdd7614d34ccd7c668b98947031c
Eh1z <godsentehiz11@gmail.com>
2025-01-25 15:07:44 +0100

    new pages

--- v2/frontend/src/App.jsx
+++ v2/frontend/src/App.jsx
@@ -2,12 +2,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./layout";
 import Welcome from "./welcome";
+import NewAccount from "./components/newAccount";
+import Login from "./components/login";
 
 const App = () => {
 	return (
 		<BrowserRouter className="w-full">
 			<Routes>
 				<Route path="/" element={<Welcome />} />
+				<Route path="/register" element={<NewAccount />} />
+				<Route path="/login" element={<Login />} />
 				<Route path="/dashboard/*" element={<Layout />} />
 			</Routes>
 		</BrowserRouter>
--- v2/frontend/src/main.jsx
+++ v2/frontend/src/main.jsx
@@ -4,11 +4,22 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ToastContainer } from "react-toastify";
+import { ClerkProvider } from "@clerk/clerk-react";
 import "react-toastify/dist/ReactToastify.css";
+
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+	throw new Error("Missing Publishable Key");
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<App />
+		<ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+			<App />
+		</ClerkProvider>
+
 		<ToastContainer position="top-right" autoClose={5000} hideProgressBar />
 	</React.StrictMode>
 );
